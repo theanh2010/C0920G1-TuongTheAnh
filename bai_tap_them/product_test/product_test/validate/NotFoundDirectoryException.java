299d8bc61afac9194d965ee00a8768f84c3c5559
theanh2010 <tuongtheanh20101997@gmail.com>
2020-11-23 19:43:20 +0700

    module2

--- bai_tap_them/product_test/product_test/validate/NotFoundDirectoryException.java
+++
@@ -1,7 +0,0 @@
-package _00_test.product_test.validate;
-
-public class NotFoundDirectoryException extends Exception {
-    public void thongBao(){
-        System.out.println("danh sach không tồn tại");
-    }
-}
--- bai_tap_them/task/NameException.java
+++
@@ -1,7 +0,0 @@
-package task;
-
-public class NameException extends Exception {
-    public void nameException(){
-        System.out.println("aaa");
-    }
-}
