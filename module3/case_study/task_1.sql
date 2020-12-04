e7c5476cc30afef85480cc2abe3b745b8e2b409f
theanh2010 <tuongtheanh20101997@gmail.com>
2020-12-04 08:58:08 +0700

    module3

--- module3/case_study/task_1.sql
+++ module3/case_study/task_1.sql
@@ -2,19 +2,28 @@ drop database if exists case_study;
 create database case_study;
 use case_study;
 create table ViTri(
-IDViTri int primary key,
+IDViTri int primary key auto_increment,
 TenViTri varchar(45));
+insert into vitri(IDViTri, TenViTri)
+values (1,'Giám Đốc'),(2,'Trưởng Phòng'),(3,'Nhân viên');
 
 create table TrinhDo(
-IDTrinhDo int primary key,
+IDTrinhDo int primary key auto_increment,
 TrinhDo varchar(45));
+insert into trinhdo (IDTrinhDo, TrinhDo)
+values (IDTrinhDo,'Đại Học'),
+(IDTrinhDo,'Cao Đẳng'), 
+(IDTrinhDo,'Trung Cấp');
 
 create table BoPhan(
-IDBoPhan int primary key,
+IDBoPhan int primary key auto_increment,
 TenBoPhan varchar(45));
 
+insert into bophan (IDBoPhan, TenBoPhan)
+values (IDBoPhan,'Điều Hành'),(IDBoPhan,'Lễ Tân'),(IDBoPhan,'Lao Công');
+
 create table NhanVien(
-IDNhanVien int primary key,
+IDNhanVien int primary key auto_increment,
 HoTen varchar(45),
 IDViTri int,
 IDTrinhDo int,
@@ -29,13 +38,21 @@ FOREIGN	KEY (IDViTri) REFERENCES ViTri(IDViTri),
 FOREIGN	KEY (IDTrinhDo) REFERENCES TrinhDo(IDTrinhDo),
 FOREIGN	KEY (IDBoPhan) REFERENCES BoPhan(IDBoPhan)
 );
-
+INSERT INTO nhanvien (IDNhanVien, HoTen, IDvitri, IDTrinhDo, IDBoPhan, NgaySinh, SoCMND, Luong, SDT, Email, DiaChi)
+VALUES (IDNhanVien,'Tưởng Thế Anh',1,1,1,'1997-12-12','111111111',500,'0966950741','abc@abc','Đà Nẵng'),
+ (IDNhanVien,'Mai Nam Khánh',1,1,1,'2008-9-2','222222222',500,'0966950741','abc@abc','Đà Nẵng'),
+ (IDNhanVien,'Phúc Khang',1,1,1,'1997-1-1','333333333',500,'0966950741','abc@abc','Quảng Nam'),
+ (IDNhanVien,'Minh Chiến',1,1,1,'1998-5-5','444444444',500,'0966950741','abc@abc','Quảng Nam'),
+ (IDNhanVien,'Đình phúc',1,1,1,'1945-6-6','5555555',500,'0966950741','abc@abc','Đà Nẵng');
+ 
 create table LoaiKhach(
-IDLoaiKhach int primary key,
+IDLoaiKhach int primary key auto_increment,
 TenLoaiKhach varchar(45));
+insert into loaikhach (IDLoaiKhach, TenLoaiKhach)
+values (1,'Sliver'),(2,'Gold'),(3,'Diamond');
 
 create table KhachHang(
-IDKhachHang int primary key,
+IDKhachHang int primary key auto_increment,
 IDLoaiKhach int,
 HoTen varchar(45),
 NgaySinh date,
@@ -45,20 +62,30 @@ Email varchar(45),
 DiaChi varchar(45),
 FOREIGN	KEY (IDLoaiKhach) REFERENCES LoaiKhach(IDLoaiKhach)
 );
+insert into khachhang(IDKhachHang, IDLoaiKhach, HoTen, NgaySinh, SoCMND, SDT, Email, DiaChi)
+values (IDKhachHang,1,'Tưởng Thế Anh','1997-10-20','123456789','0977555444','anh@gmail.com','Đà Nẵng'),
+ (IDKhachHang,2,'Nguyễn Minh Chiến','1997-11-11','123456789','0977666444','chien@gmail.com','Quảng Bình'),
+ (IDKhachHang,3,'Nguyễn Đình Phúc','1997-8-8','123456789','0977335444','anh@gmail.com','Đà Nẵng');
 
 create table KieuThue(
-IDKieuThue int primary key,
+IDKieuThue int primary key auto_increment,
 TenKieuThue varchar(45),
 Gia int
 );
+insert into kieuthue(IDKieuThue, TenKieuThue, Gia)
+values (IDKieuThue,'giờ',50),
+(IDKieuThue,'ngày',300),
+(IDKieuThue,'tháng',1000);
 
 create table LoaiDichVu(
-IDLoaiDichVu int primary key,
+IDLoaiDichVu int primary key auto_increment,
 TenLoaiDichVu varchar(45)
 );
+insert into loaidichvu(IDLoaiDichVu, TenLoaiDichVu)
+values (1,'Villa'),(2,'Room'),(3,'House');
 
 create table DichVu(
-IDDichVu int primary key,
+IDDichVu int primary key auto_increment,
 TenDichVu varchar(45),
 DienTich int,
 SoTang int,
@@ -69,8 +96,14 @@ TrangThai varchar(45),
 FOREIGN	KEY (IDKieuThue) REFERENCES KieuThue(IDKieuThue),
 FOREIGN	KEY (IDLoaiDichVu) REFERENCES LoaiDichVu(IDLoaiDichVu)
 );
+
+insert into dichvu (IDDichVu, TenDichVu, DienTich, SoTang, SoNguoiToiDa, IDKieuThue, IDLoaiDichVu, TrangThai)
+values (IDDichVu,'Villa',300,4,12,IDKieuThue,IDLoaiDichVu,"ok"),
+		(IDDichVu,'House',100,2,3,IDKieuThue,IDLoaiDichVu,'ok'),
+        (IDDichVu,'Room',50,1,2,IDKieuThue,IDLoaiDichVu,'ok');
+
 create table HopDong(
-IDHopDong int primary key,
+IDHopDong int primary key auto_increment,
 IDNhanVien int,
 IDKhachHang int,
 IDDichVu int,
@@ -82,16 +115,26 @@ FOREIGN	KEY (IDNhanVien) REFERENCES NhanVien(IDNhanVien),
 FOREIGN	KEY (IDKhachHang) REFERENCES KhachHang(IDKhachHang),
 FOREIGN	KEY (IDDichVu) REFERENCES DichVu(IDDichVu)
  );
+ 
+ insert into hopdong(IDHopDong, IDNhanVien, IDKhachHang, IDDichVu, NgayLamHopDong, NgayKetThucHopDong, TienDatCoc)
+values(IDHopDong,1,1,1,'2020-08-03','2020-12-3',200),
+(IDHopDong,2,2,2,'2020-12-12','2020-12-24',500),
+(IDHopDong,3,3,3,'2020-12-7','2020-12-27',1000);
+
 
 create table DichVuDiKem(
-IDDichVuDiKem int primary key,
+IDDichVuDiKem int primary key auto_increment,
 TenDichVuDiKem varchar(45),
 Gia int,
 DonVi int,
 TrangThaiKhaDung varchar(45));
+insert into dichvudikem(IDDichVuDiKem, TenDichVuDiKem, Gia, DonVi, TrangThaiKhaDung)
+values(IDDichVuDiKem,'karaoke',300,12,'chục em chân dài'),
+(IDDichVuDiKem,'massage',100,2,'chục em tiếp viên'),
+(IDDichVuDiKem,'hồ bơi',1000,10,'hồ cao 1m2');
 
 create table HopDongChiTiet(
-IDHopDongChiTiet int,
+IDHopDongChiTiet int primary key auto_increment,
 IDHopDong int,
 IDDichVuDiKem int,
 so_luong int,
@@ -99,52 +142,31 @@ FOREIGN	KEY (IDHopDong) REFERENCES HopDong(IDHopDong),
 FOREIGN	KEY (IDDichVuDiKem) REFERENCES DichVuDiKem(IDDichVuDiKem)
 );
 
-insert into bophan (IDBoPhan, TenBoPhan)
-values (1,'Điều Hành'),(2,'Lễ Tân');
-
-insert into dichvu (IDDichVu, TenDichVu, DienTich, SoTang, SoNguoiToiDa, IDKieuThue, IDLoaiDichVu, TrangThai)
-values (1,'Villa',300,4,12,1,1,"ok"),
-		(2,'House',100,2,3,2,2,'ok'),
-        (3,'Room',50,1,2,3,3,'ok');
-
-insert into dichvudikem(IDDichVuDiKem, TenDichVuDiKem, Gia, DonVi, TrangThaiKhaDung)
-values (3,'hồ bơi',1000,10,'hồ cao 1m2');
-
-
-insert into hopdong(IDHopDong, IDNhanVien, IDKhachHang, IDDichVu, NgayLamHopDong, NgayKetThucHopDong, TienDatCoc)
-values(1,1,1,1,8/3/2020,12/3/2020,200),
-(2,2,2,2,12/12/2020,24/12/2020,500),
-(3,3,3,3,10/12/2020,27/12/2020,1000);
-
 insert into hopdongchitiet(IDHopDongChiTiet, IDHopDong, IDDichVuDiKem, so_luong)
-values(1,1,1,10),(2,2,2,11),(3,3,3,12);
+values(IDHopDongChiTiet,1,1,10),
+(IDHopDongChiTiet,2,2,11),
+(IDHopDongChiTiet,3,3,12);
 
-insert into khachhang(IDKhachHang, IDLoaiKhach, HoTen, NgaySinh, SoCMND, SDT, Email, DiaChi)
-values (1,1,'Tưởng Thế Anh',20/10/1997,'123456789','0977555444','anh@gmail.com','Đà Nẵng'),
- (2,2,'Nguyễn Minh Chiến',12/12/1997,'123456789','0977666444','chien@gmail.com','Quảng Bình'),
- (3,3,'Nguyễn Đình Phúc',11/11/1997,'123456789','0977335444','anh@gmail.com','Đà Nẵng');
 
-insert into kieuthue(IDKieuThue, TenKieuThue, Gia)
-values (1,'giờ',50),(2,'ngày',300),(3,'tháng',1000);
 
-insert into loaidichvu(IDLoaiDichVu, TenLoaiDichVu)
-values (1,'Villa'),(2,'Room'),(3,'House');
 
-insert into loaikhach (IDLoaiKhach, TenLoaiKhach)
-values (1,'Sliver'),(2,'Gold'),(3,'Diamond');
 
-INSERT INTO nhanvien (IDNhanVien, HoTen, IDvitri, IDTrinhDo, IDBoPhan, NgaySinh, SoCMND, Luong, SDT, Email, DiaChi)
-VALUES (1,'Tưởng Thế Anh',1,1,1,20/10/1997,'111111111',500,'0966950741','abc@abc','Đà Nẵng'),
- (2,'Mai Nam Khánh',1,1,1,20/10/2008,'222222222',500,'0966950741','abc@abc','Đà Nẵng'),
- (3,'Phúc Khang',1,1,1,20/10/1997,'333333333',500,'0966950741','abc@abc','Đà Nẵng'),
- (4,'Minh Chiến',1,1,1,20/10/1993,'444444444',500,'0966950741','abc@abc','Đà Nẵng'),
- (5,'Đình phúc',1,1,1,20/10/1992,'5555555',500,'0966950741','abc@abc','Đà Nẵng');
- 
-insert into trinhdo (IDTrinhDo, TrinhDo)
-values (3,'Trung Cấp');
-update trinhdo
-set TrinhDo = 'Cao Đẳng'
-where IDTrinhDo = 2;
 
-insert into vitri(IDViTri, TenViTri)
-values (1,'Giám Đốc'),(2,'Trưởng Phòng'),(3,'Nhân viên');
+
+
+
+
+
+
+
+
+
+
+select * from nhanvien
+where (HoTen like '%H%' or HoTen like '%T%')
+and length(HoTen) < 15;
+
+select * from nhanvien
+where year(curdate()) - year(NgaySinh) > 18 
+and year(curdate()) - year(NgaySinh) < 50 
+and DiaChi in ('Đà Nẵng', 'Quảng Nam');
